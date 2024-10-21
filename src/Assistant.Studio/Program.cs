03adb232710ae2d3a9959fc4defa9f583b3abb74
afroze9 <afrozeamjad@gmail.com>
2024-10-21 22:49:57 +0500

    Basics running

--- src/Assistant.Studio/Program.cs
+++ src/Assistant.Studio/Program.cs
@@ -1,31 +1,59 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
-using Assistant.Studio.Data;
+using Elsa.Studio.Core.BlazorServer.Extensions;
+using Elsa.Studio.Dashboard.Extensions;
+using Elsa.Studio.Extensions;
+using Elsa.Studio.Login.BlazorServer.Extensions;
+using Elsa.Studio.Login.HttpMessageHandlers;
+using Elsa.Studio.Shell.Extensions;
+using Elsa.Studio.Workflows.Designer.Extensions;
+using Elsa.Studio.Workflows.Extensions;
 
-WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
 
-// Add services to the container.
+// Register Razor services.
 builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddServerSideBlazor(options =>
+{
+    // Register the root components.
+    options.RootComponents.RegisterCustomElsaStudioElements();
+});
 
-WebApplication app = builder.Build();
+// Register shell services and modules.
+builder.Services.AddCore();
+builder.Services.AddShell(options => configuration.GetSection("Shell").Bind(options));
+builder.Services.AddRemoteBackend(
+    elsaClient => elsaClient.AuthenticationHandler = typeof(AuthenticatingApiHttpMessageHandler),
+    options => configuration.GetSection("Backend").Bind(options));
+builder.Services.AddLoginModule();
+builder.Services.AddDashboardModule();
+builder.Services.AddWorkflowsModule();
+
+// Configure SignalR.
+builder.Services.AddSignalR(options =>
+{
+    // Set MaximumReceiveMessageSize to handle large workflows.
+    options.MaximumReceiveMessageSize = 5 * 1024 * 1000; // 5MB
+});
+
+var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+    app.UseResponseCompression();
+
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
-
-app.UseStaticFiles();
-
-app.UseRouting();
+app
+    .UseHttpsRedirection()
+    .UseStaticFiles()
+    .UseRouting()
+    .UseAuthorization();
 
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 
+// Run the application.
 app.Run();
