f83f2713d7fd972060bf6603983585638ef21c4b
afroze9 <afrozeamjad@gmail.com>
2024-11-09 17:18:46 +0500

    [WIP] Switching to BlazorHybrid

--- src/Assistant.Desktop/MainWindow.xaml.cs
+++ src/Assistant.Desktop/MainWindow.xaml.cs
@@ -1,125 +1,17 @@
-﻿using System.IO;
-using System.Windows;
-using System.Windows.Input;
+﻿using System.Windows;
 
-using Assistant.Desktop.Configuration;
-using Assistant.Desktop.Services;
-using Assistant.Desktop.ViewModels;
-
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using Microsoft.Identity.Client;
-using Microsoft.SemanticKernel;
-using Microsoft.SemanticKernel.Connectors.OpenAI;
-using Microsoft.SemanticKernel.TextToAudio;
-
-using NAudio.Wave;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Assistant.Desktop;
 
 public partial class MainWindow : Window
 {
-    private readonly IAuthService _authService;
-    private readonly IAiService _aiService;
-    private readonly ITextToAudioService _textToAudioService;
-    private readonly ILogger<MainWindow> _logger;
-    private readonly MainWindowViewModel _viewModel;
-    private readonly SKAssistantOptions _skAssistantOptions;
+    public IServiceProvider Services { get; }
 
-    public MainWindow(
-        IAuthService authService,
-        IAiService aiService,
-        ITextToAudioService textToAudioService,
-        IOptions<SKAssistantOptions> skAssistantOptions,
-        ILogger<MainWindow> logger)
+    public MainWindow(IServiceProvider services)
     {
-        _authService = authService;
-        _aiService = aiService;
-        _textToAudioService = textToAudioService;
-        _logger = logger;
-        _skAssistantOptions = skAssistantOptions.Value;
-
-        _viewModel = new MainWindowViewModel();
+        Services = services;
         InitializeComponent();
-
-        DataContext = _viewModel;
-    }
-
-    private async void LoginButton_OnClick(object sender, RoutedEventArgs e)
-    {
-        AuthenticationResult? result = await _authService.SignInUserAsync();
-
-        if (result != null)
-        {
-            _logger.LogInformation("Logged in with {UserName}", result.Account.Username);
-        }
-    }
-
-    private void ChatInputTextBox_OnKeyDown(object sender, KeyEventArgs e)
-    {
-        if (e.Key == Key.Enter)
-        {
-            SendButton_OnClick(sender, e);
-        }
-    }
-
-    private async void SendButton_OnClick(object sender, RoutedEventArgs e)
-    {
-        if (!string.IsNullOrWhiteSpace(ChatInputTextBox.Text))
-        {
-            _viewModel.ChatMessages.Add(new ChatMessageViewModel
-            {
-                Message = ChatInputTextBox.Text.Trim(), Role = "User",
-            });
-
-            await Task.Run(() =>
-            {
-                Application.Current.Dispatcher.Invoke(() =>
-                {
-                    ChatInputTextBox.Clear();
-                    ChatInputTextBox.Focus();
-                    ChatListBox.ScrollIntoView(_viewModel.ChatMessages.Last());
-                    SendButton.IsEnabled = false;
-                });
-            });
-
-            await Task.Run(async () =>
-            {
-                ChatMessageViewModel response = await _aiService.GenerateAsync(_viewModel.ChatMessages.ToList());
-                AudioContent? audio = null;
-                
-                if (_skAssistantOptions.EnableAudio)
-                {
-                    audio = await _textToAudioService.GetAudioContentAsync(response.Message,
-                        new OpenAITextToAudioExecutionSettings() { Voice = "nova", ResponseFormat = "mp3", });
-                }
-
-                Application.Current.Dispatcher.Invoke(() =>
-                {
-                    _viewModel.ChatMessages.Add(response);
-                    ChatListBox.ScrollIntoView(_viewModel.ChatMessages.Last());
-                    SendButton.IsEnabled = true;
-                });
-
-                if (_skAssistantOptions.EnableAudio && audio != null)
-                {
-                    Application.Current.Dispatcher.InvokeAsync(async () =>
-                    {
-                        using (var stream = new MemoryStream(audio.Data?.ToArray() ?? []))
-                        await using (var reader = new Mp3FileReader(stream))
-                        using (var waveOut = new WaveOutEvent())
-                        {
-                            waveOut.Init(reader);
-                            waveOut.Play();
-
-                            while (waveOut.PlaybackState == PlaybackState.Playing)
-                            {
-                                await Task.Delay(100);
-                            }
-                        }
-                    });
-                }
-            });
-        }
+        DataContext = this;
     }
 }
